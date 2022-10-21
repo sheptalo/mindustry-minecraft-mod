bd8dd45d0e29af29a9e7c54233fefb342b70bab0
sheptalo <solovovartem99@gmail.com>
2022-10-21 18:55:59 +0300

    qwerty

--- src/minecraft/content/MinecraftBlocks.java
+++ src/minecraft/content/MinecraftBlocks.java
@@ -38,7 +38,7 @@ public class MinecraftBlocks{
 //environment
 bedrock, cactus, clay, coal_ore, coarse_dirt,
 cobblestone_mosy, cobblestone, diamond_ore,
-emerald_ore, gold_ore, grass_patl, gravel, 
+emerald_ore, gold_ore, grass_path, gravel, 
 hay_block, iron_ore, lapis_ore, lava_stil, 
 log_oak, redstone_ore, snow, stone_andesite, 
 stone_diorite, stone_granite, stone, water_still, farmland, web,
@@ -51,8 +51,9 @@ stone_andesite_smooth, stone_diorite_smooth, stone_granite_smooth,
 stonebrick_carved, stonebrick_cracked, stonebrick_mossy, stonebrick,
 tnt, 
 //craftblocks
-  crafting_table, furnace;
-  
+  crafting_table, furnace,
+  //pickaxe,axe,shovel
+  wooden_pickaxe;
   public static void load(){
   farmland = new Floor("farmland"){{
     requirements(Category.production, ItemStack.with(Items.copper,1));
@@ -78,10 +79,11 @@ tnt,
   oreThreshold = 25.4F;
   oreScale = 0.3F;
   wallOre = true;
-  itemDrop = MinecraftItems.clay;
+  itemDrop = MinecraftItems.coal;
   localizedName = itemDrop.localizedName;
   mapColor.set(itemDrop.color);
   useColor = true;
+  Attribute.add("pickaxe");
   }};
   coarse_dirt = new Floor("coarse_dirt");
   cobblestone_mosy = new StaticWall("cobblestone_mosy");
@@ -99,6 +101,7 @@ tnt,
   localizedName = itemDrop.localizedName;
   mapColor.set(itemDrop.color);
   useColor = true;
+  Attribute.add("pickaxe");
   }};
   emerald_ore = new OreBlock("emerald_ore"){{
     oreDefault = true;
@@ -110,6 +113,7 @@ tnt,
   localizedName = itemDrop.localizedName;
   mapColor.set(itemDrop.color);
   useColor = true;
+  Attribute.add("pickaxe");
   }};
   gold_ore = new OreBlock("gold_ore"){{
     oreDefault = true;
@@ -117,13 +121,47 @@ tnt,
   oreThreshold = 25.4F;
   oreScale = 0.3F;
   wallOre = true;
-  itemDrop = MinecraftItems.gold__block;
+  itemDrop = MinecraftItems.gold_ore;
   localizedName = itemDrop.localizedName;
   mapColor.set(itemDrop.color);
   useColor = true;
+  Attribute.add("pickaxe");
   }};
-
-
+  grass_path = new Floor("grass_path"){{
+    speedMultiplier = 2f;
+  }};
+hay_block = new StaticWall("hay_block");
+iron_ore = new OreBlock("iron_ore"){{
+  oreDefault = true;
+  variants = 0;
+oreThreshold = 25.4F;
+oreScale = 0.3F;
+wallOre = true;
+itemDrop = MinecraftItems.iron_ore;
+localizedName = itemDrop.localizedName;
+mapColor.set(itemDrop.color);
+useColor = true;
+Attribute.add("pickaxe");
+}};
+lapis_ore = new OreBlock("lapis_ore"){{
+  oreDefault = true;
+  variants = 0;
+oreThreshold = 25.4F;
+oreScale = 0.3F;
+wallOre = true;
+itemDrop = MinecraftItems.lapis;
+localizedName = itemDrop.localizedName;
+mapColor.set(itemDrop.color);
+useColor = true;
+Attribute.add("pickaxe");
+}};
+wooden_pickaxe = new BeamDrill("wooden_pickaxe"){{
+  Attribute.get("pickaxe");
+  drillTime = 300f;
+  tier = 1;
+  size = 1;
+  range = 1;
+}};
 
 
 
@@ -133,8 +171,10 @@ tnt,
 
 
   //TODO import all crafts.
-  crafting_table = new MultiCrafter("crafting_table"){{
+  //crash because null recipes
+ /*  crafting_table = new MultiCrafter("crafting_table"){{
+  requirement(Category.production, ItemStack.with(MinecraftItems.planks, 4));
     size  = 1;
-  }};
+  }};*/
   }
 }
--- src/minecraft/content/MinecraftItems.java
+++ src/minecraft/content/MinecraftItems.java
@@ -2,12 +2,15 @@ package minecraft.content;
 import arc.graphics.Color;
 import mindustry.type.Item;;
 public class MinecraftItems {
-    public static Item clay,emerald,gold__block,diamond;
+    public static Item clay,emerald,gold_ore,iron_ore,coal,lapis,diamond;
 
     public static void load(){
         clay = new Item("clay", Color.lightGray){{hardness = 1;}};
         diamond = new Item("diamond",Color.blue){{hardness = 3;}};
         emerald = new Item("emerald",Color.green){{hardness = 3;}};
-        gold__block = new Item("gold__block",Color.gold){{hardness = 3;}};
+        gold_ore = new Item("gold_ore",Color.gold){{hardness = 3;}};
+        iron_ore = new Item("iron_ore",Color.gray){{hardness = 2;}};
+        coal = new Item("coal",Color.black){{hardness = 1;}};
+        lapis = new Item("lapis", Color.blue){{hardness = 2;}};
     }
 }
